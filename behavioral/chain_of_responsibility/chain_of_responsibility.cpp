9c1013229bc754be49cf4d0580df5a5654de602a
smartnlgilianovikov <smartnlg.ilia.novikov@gmail.com>
2018-06-10 14:27:39 +0300

    behavioral: chain of resposibility

---
+++ behavioral/chain_of_responsibility/chain_of_responsibility.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+class IChainOfResponsibility{
+public:
+	virtual void process( int i) = 0;
+	void setNext( IChainOfResponsibility* next){ next_ = next; } 
+	
+protected:
+	IChainOfResponsibility* next_;
+};
+
+class PositiveNumberHandler: public IChainOfResponsibility{
+public:
+	void process( int i) 
+	{ 
+		if( i > 0)
+			std::cout << "Positive" << std::endl;
+		else
+			next_->process( i);
+	}
+};
+
+class NegativeNumberHandler: public IChainOfResponsibility{
+public:
+	void process( int i) 
+	{ 
+		if( i < 0)
+			std::cout << "Negative" << std::endl;
+		else
+			next_->process( i);
+	}
+};
+
+class ZeroNumberHandler: public IChainOfResponsibility{
+public:
+	void process( int i) 
+	{ 
+		if( i == 0)
+			std::cout << "Zero" << std::endl;
+		else
+			next_->process( i);
+	}
+};
+
+int main()
+{
+	IChainOfResponsibility* chain = new PositiveNumberHandler();
+	IChainOfResponsibility* second = new NegativeNumberHandler();
+	IChainOfResponsibility* third = new ZeroNumberHandler();
+	
+	chain->setNext( second);
+	second->setNext( third);
+	
+	chain->process( 1);
+	chain->process( -1);
+	chain->process( 0);
+}
